8eac358a84f5051b3d578b3f0ab991c7672c8544
Michael Malchak <michaelmalchak@gmail.com>
2015-01-16 21:03:54 -0800

    Add the simplest implementation of ember-notify

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('bower_components/foundation/css/normalize.css');
+app.import('bower_components/foundation/css/foundation.css');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
