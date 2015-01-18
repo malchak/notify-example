d8443656df7d9a68a957da4e91c41992852f7358
Michael Malchak <michaelmalchak@gmail.com>
2015-01-17 19:14:52 -0800

    Add example for info, warning, and alert notify messages

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
 	actions: {
-		foo: function () {
+
+		getInfo: function () {
+			this.notify.info('This is and ember-notify message');
+		},
+
+		clickMe: function () {
 			this.notify.success('Hello there!');
+		},
+
+		doNotClickMe: function () {
+			this.notify.warning('Great... now you\'ve done it.');
+		},
+
+		somthingBad: function () {
+			this.notify.alert('You better go double check your account balance.');
 		}
 	}
+
 });
